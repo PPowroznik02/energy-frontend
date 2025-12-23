43857bfcec602c275e31f0fb16b9d653eadd6dcd
PPowroznik02 <ppowroznik02@gmail.com>
2025-12-23 02:16:58 +0100

    Add frontend of energy app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,4 @@
+import './App.css'
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,22 @@
-import Link from "next/link";
+import {useRouter} from "next/router";
 
 export default function Home() {
-  return (
-    <div>
-      Hello World.{" "}
-      <Link href="/about">
-        About
-      </Link>
-    </div>
-  );
+    const router = useRouter();
+
+    return (
+        <>
+            <h1>UK Energy App</h1>
+
+            <section>
+                <button onClick={() => router.push("/energy/EnergyDashboard")}>
+                    Energy mix
+                </button>
+
+                <button onClick={() => router.push("/energy/ChargingWindow")}>
+                    Charging window
+                </button>
+            </section>
+
+        </>
+    )
 }
